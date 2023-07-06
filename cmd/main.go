d47b171f12a6a9980354965606ea21fa36487a06
be3 <d_0711@icloud.com>
2023-07-06 20:14:35 +0900

    fix: redefine config for connection

--- cmd/main.go
+++ cmd/main.go
@@ -6,23 +6,21 @@ import (
 	"time"
 
 	"github.com/Be3751/socket-capture-signals/internal/agent"
-	"github.com/Be3751/socket-capture-signals/internal/config"
 	"github.com/Be3751/socket-capture-signals/internal/pkg/socket"
 )
 
 // TODO: 受信コマンドをParseする関数も必要
 // TODO: クライアント側のIPアドレスを自動取得する処理も必要
 func main() {
-	conf := config.Config{
-		ServerIP:         "127.0.0.1",
-		ServerPortText:   "3000",
-		ServerPortBinary: "2000",
-		ClientIP:         "127.0.0.1",
-		ClientPort:       1000,
+	conf := socket.Config{
+		ServerIP:   "192.168.86.24",
+		ServerPort: "3000",
+		ClientIP:   "192.168.86.21",
+		ClientPort: 1000,
 	}
 	ctx := context.Background()
 
-	conn, err := socket.NewClient(conf)
+	conn, err := socket.Connect(conf)
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
@@ -38,7 +36,7 @@ func main() {
 	}
 
 	buf := make([]byte, 1024)
-	err = agent.CaptureSig(ctx, buf)
+	err = agent.StartRec(ctx, time.Minute, "2023/04/17 12:12:12")
 	if err != nil {
 		fmt.Println(err)
 		panic(err)
--- internal/config/config.go
+++
@@ -1,9 +0,0 @@
-package config
-
-type Config struct {
-	ServerIP         string
-	ServerPortText   string
-	ServerPortBinary string
-	ClientIP         string
-	ClientPort       int
-}
