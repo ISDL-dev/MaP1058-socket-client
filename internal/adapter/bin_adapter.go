10d9f595480fc1573a03acbc204c6178e454e273
be3 <49334354+Be3751@users.noreply.github.com>
2023-08-04 01:31:59 +0900

    Revert "SetMeasurementsをテスト"

--- internal/adapter/bin_adapter.go
+++ internal/adapter/bin_adapter.go
@@ -34,7 +34,7 @@ func (a *adapter) ReceiveADValues(ctx context.Context) (*model.Signals, error) {
 		return nil, fmt.Errorf("failed to receive binary data %w", err)
 	}
 	signals := model.NewSignals()
-	err = a.Parser.ToSignals(rawBytes, signals)
+	err = a.Parser.ToSignals(rawBytes, &signals)
 	if err != nil {
 		if e, ok := err.(*parser.FailureSumCheckError); ok {
 			if err := a.sendNAK(); err != nil {
@@ -47,7 +47,7 @@ func (a *adapter) ReceiveADValues(ctx context.Context) (*model.Signals, error) {
 	if err != nil {
 		return nil, err
 	}
-	return signals, nil
+	return &signals, nil
 }
 
 func (a *adapter) sendACK() error {
