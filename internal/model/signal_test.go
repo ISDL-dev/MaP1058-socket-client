88e280975e3078438fcf96a338d76b9b90b161d5
be3 <d_0711@icloud.com>
2023-08-04 02:17:18 +0900

    Revert "Merge branch 'main' into feature/socket"
    This reverts commit 2a554013162aaa7fdb6a7750abb0e2355a7979d5, reversing
    changes made to 3ac20ab094f943307a35f8a2c495818f03081612.

---
+++ internal/model/signal_test.go
@@ -0,0 +1,55 @@
+package model
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetMeasurements(t *testing.T) {
+	t.Run("計測値をセット", func(t *testing.T) {
+		signals := NewSignals()
+		for ch := range signals.Channels {
+			for pnt := range signals.Channels[ch].ADValues {
+				signals.Channels[ch].ADValues[pnt] = 1
+			}
+		}
+		cal := Cal{
+			{1, 1, 0, 2},
+			{1, 1, 0, 0},
+			{1, 1, 0, 0},
+			{1, 1, 0, 0},
+			{1, 1, 0, 0},
+			{1, 1, 0, 0},
+			{1, 1, 0, 0},
+			{1, 1, 0, 0},
+		}
+		err := signals.SetMeasurements(cal)
+		assert.NoError(t, err)
+		for ch := range signals.Channels {
+			for pnt := range signals.Channels[ch].Measurements {
+				if ch == 0 {
+					assert.Equal(t, float64(2), signals.Channels[ch].Measurements[pnt])
+				} else {
+					assert.Equal(t, float64(0), signals.Channels[ch].Measurements[pnt])
+				}
+			}
+		}
+	})
+
+	t.Run("CAL_ADの値が0のCalを受け取ってエラー", func(t *testing.T) {
+		signals := NewSignals()
+		cal := Cal{
+			{1, 0, 0, 2},
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+			{1, 0, 0, 0},
+		}
+		err := signals.SetMeasurements(cal)
+		assert.Error(t, err)
+	})
+}
