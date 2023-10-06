c35a9d825dc3399456f9d33d67ac6168fa7dd311
be3 <d_0711@icloud.com>
2023-10-06 22:50:42 +0900

    WIP

---
+++ internal/client/writer.go
@@ -0,0 +1,15 @@
+package client
+
+import "os"
+
+func (c *client) writeSetting(set interface{}, f *os.File) error {
+	return nil
+}
+
+func (c *client) writeRaw(raw interface{}, f *os.File) error {
+	return nil
+}
+
+func (c *client) writeAnalyzed(txt interface{}, f *os.File) error {
+	return nil
+}
