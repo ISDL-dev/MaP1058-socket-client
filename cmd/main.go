86f32bbf4eb4b366c9c06b37b0dca13213420cd6
Be3751 <d_0711@icloud.com>
2023-10-21 19:58:32 +0900

    add goroutine, writer

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/csv"
 	"fmt"
 	"os"
 	"time"
@@ -21,7 +22,7 @@ func main() {
 		panic(err)
 	}
 	// TODO: サーバー側のIPアドレスを動的に取得する処理も必要
-	serverIP := "192.168.10.105"
+	serverIP := "192.168.10.101"
 	txtAdConf := socket.SocketConfig{
 		ServerIP:   serverIP,
 		ServerPort: "3000",
@@ -56,48 +57,60 @@ func main() {
 		}
 	}()
 
-	ctx := context.Background()
+	ctx, cancel := context.WithCancel(context.Background())
 	parser := parser.NewParser()
 	scanner := scanner.NewCustomScanner(txtAdConn)
 	txtAdapter := adapter.NewTxtAdapter(txtAdConn, scanner, parser)
-	binAdapter := adapter.NewBinAdapter(binAdConn, parser)
+	// binAdapter := adapter.NewBinAdapter(binAdConn, parser)
+	eegWriter := csv.NewWriter(os.Stdout)
+	writerGroup := adapter.CSVWriterGroup{
+		EEGWriter: *eegWriter,
+	}
+	defer func() {
+		eegWriter.Flush()
+	}()
 
 	err = txtAdapter.StartRec(ctx, time.Second*60, time.Now())
 	if err != nil {
 		panic(err)
 	}
-	defer func() {
-		err = txtAdapter.EndRec(ctx)
-		if err != nil {
-			panic(err)
-		}
-		os.Exit(0)
-	}()
+	// defer func() {
+	// 	err = txtAdapter.EndRec(ctx)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
+	// 	os.Exit(0)
+	// }()
 
+	// TODO: parseする前の受信コマンドを実際に確認する（parserにバグがある可能性）
 	setting, err := txtAdapter.GetSetting()
 	if err != nil {
 		panic(err)
 	}
 	// TODO: 設定値をファイルに書き込む
+	for {
+		tmp := make([]byte, 128)
+		_, err := txtAdConn.Read(tmp)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(tmp))
+	}
 
 	// TODO: 生波形の受信と解析データの受信を並行して行う
 	// TODO: ENDコマンドを受信するまで、生波形の受信とファイル書き込みを繰り返す
-	for i := 0; i < 10; i++ {
-		s, err := binAdapter.ReceiveADValues(ctx)
+	go func() {
+		time.Sleep(time.Second * 2)
+		cancel()
+	}()
+	go func() {
+		fmt.Println("start to get trend data")
+		err := txtAdapter.GetTrendData(ctx, writerGroup, setting.AnalysisType)
 		if err != nil {
 			panic(err)
 		}
-		err = s.SetMeasurements(setting.Calibration)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Println(s)
-		// // TODO: 計測値をファイルに書き込む
-		// _, err = file.WriteString()
-		// if err != nil {
-		// 	panic(err)
-		// }
-	}
+	}()
+	time.Sleep(time.Second * 5)
 
 	// TODO: ENDコマンドを受信するまで、解析データの受信とファイル書き込みを繰り返す
 	// TODO: 計測値をファイルに書き込む
